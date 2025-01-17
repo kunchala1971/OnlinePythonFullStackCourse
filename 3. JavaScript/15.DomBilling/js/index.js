bb944d32195a841759fd4dfa34b9bffee1d56e01
kunchala1971 <kunchala1971@gmail.com>
2025-01-17 10:19:21 +0530

    today updates

--- 3. JavaScript/15.DomBilling/js/index.js
+++ 3. JavaScript/15.DomBilling/js/index.js
@@ -5,6 +5,9 @@ let taxAmountElement=document.getElementById("tax_percent");
 let amountElement=document.getElementById("amount");
 let tipAmountElement=document.getElementById("tip_amount");
 let netBillAmountElement=document.getElementById("net_bill_amount");
+let bill_split_nosElement=document.getElementById("bill_split_nos");
+let for_each_amountElement=document.getElementById("for_each_amount");
+
 function submitHandler()
 {
 	alert("Payment is Over");
@@ -12,18 +15,25 @@ function submitHandler()
 function getNetAmount()
 {
 let net_amount=parseInt(grossBillAmountElement.value)-parseInt(discountAmountElement.value);
-netAmountElement.value=net_amount;
+netAmountElement.value=parseInt(net_amount);
 }
 function getAmount()
 {
 	let net_amount=parseInt(grossBillAmountElement.value)-parseInt(discountAmountElement.value);
 	let amount=net_amount+(net_amount*parseInt(taxAmountElement.value))/100;
-	amountElement.value=amount;
+	amountElement.value=parseInt(amount);
 }
 function getNetBillAmount()
 {
 	let net_amount=parseInt(grossBillAmountElement.value)-parseInt(discountAmountElement.value);
 	let amount=net_amount+(net_amount*parseInt(taxAmountElement.value))/100;
 	let netBillAmount=amount+parseInt(tipAmountElement.value);
-	netBillAmountElement.value=netBillAmount;
+	netBillAmountElement.value=parseInt(netBillAmount);
+}
+function getSplitAmount()
+{
+	let net_amount=parseInt(netBillAmountElement.value);
+	let forEachAmount=parseInt(net_amount/parseInt(bill_split_nosElement.value));
+	for_each_amountElement.value=parseInt(forEachAmount);
+
 }
